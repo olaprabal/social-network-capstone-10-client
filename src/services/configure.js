2eec55fd7de538446628a2926f653642b2734764
Prabal Tikeriha <prabal.tikeriha@olacabs.com>
2022-07-31 18:55:19 +0530

    baseurl changed

--- src/services/configure.js
+++ src/services/configure.js
@@ -1,3 +1,3 @@
-export const BaseUrl = 'https://social-app-group10.herokuapp.com'
+export const BaseUrl = 'https://social-media-app-10.herokuapp.com'
 // export const BaseUrl = 'http://localhost:8080'
 export const BaseUrlInner = '/api/capstone'
